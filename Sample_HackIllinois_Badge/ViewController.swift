1176a919df5b1bf55aad6bfd1eaadaaaa12251fe
hsahn0323 <hyosang2@illinois.edu>
2019-09-18 21:23:52 -0500

    Initial Commit

---
+++ Sample_HackIllinois_Badge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample_HackIllinois_Badge
+//
+//  Created by Hyosang Ahn on 9/18/19.
+//  Copyright © 2019 Hyosang Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

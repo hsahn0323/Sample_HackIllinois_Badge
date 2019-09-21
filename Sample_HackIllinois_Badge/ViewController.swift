c2e0ec1060cfcefdc3a97060b91b3b49afd5492a
hsahn0323 <hyosang2@illinois.edu>
2019-09-21 12:05:57 -0500

    Enhanced UI design. Disabled screen rotation.

--- Sample_HackIllinois_Badge/ViewController.swift
+++ Sample_HackIllinois_Badge/ViewController.swift
@@ -8,66 +8,23 @@
 
 import UIKit
 
-//struct EventResponse:Decodable {
-//    var response:Events
-//}
-//
-//struct Events:Decodable {
-//    var events:[EventDetail]
-//}
-//
-//
-//
-//struct EventDetail:Decodable {
-//    var name:String
-//    var location:EventLocation
-//}
-//
-//
-//
-//struct EventLocation:Decodable {
-//    var latitude:Double
-//    var longitude:Double
-//}
-
 class ViewController: UIViewController {
     //MARK: Properties
     @IBOutlet weak var labelLatitude: UILabel!
     @IBOutlet weak var labelLongitude: UILabel!
     
     var eventList = [EventDetail]()
-//    {
-//        didSet {
-//            DispatchQueue.main.async {
-//                self.labelLatitude
-//            }
-//        }
-//    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        let info = "Event 2"
-//        let eventRequest = EventRequest()
-//        eventRequest.getEvents { [weak self] result in
-//            switch result {
-//            case .failure(let error):
-//                print(error)
-//            case .success(let events):
-//                self?.eventList = events
-//
-//            }
-//        }
-//        self.labelLatitude.text = "\(self.eventList.count)"
-//        self.labelLatitude.text = "Latitude: \(self.eventList[1].location.latitude)"
-//        self.labelLongitude.text = "Longitude: \(self.eventList[1].location.longitude)"
         
         fetchEventsJSON { (res) in
             switch res {
             case .success(let events):
                 DispatchQueue.main.async
-                    {self.labelLatitude.text = "Latitude: \(events.events[1].locations[0].latitude)"
-                    self.labelLongitude.text = "Longitude: \(events.events[1].locations[0].longitude)"}
+                    {self.labelLatitude.text = "\(events.events[1].locations[0].latitude)"
+                    self.labelLongitude.text = "\(events.events[1].locations[0].longitude)"}
             case .failure(let err):
                 print("Failed to fetch events: ", err)
             }
@@ -79,13 +36,11 @@ class ViewController: UIViewController {
         guard let url = URL(string: urlString) else {
             return
         }
-        
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
             if let err = err {
                 completion(.failure(err))
                 return
             }
-            
             do {
                 let events = try JSONDecoder().decode(Events.self, from: data!)
                 completion(.success(events))
@@ -93,9 +48,112 @@ class ViewController: UIViewController {
                 completion(.failure(jsonError))
             }
         }.resume()
-        
-        
     }
-
+    
+    override open var shouldAutorotate: Bool {
+        return false
+    }
 }
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+}
+
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
 
